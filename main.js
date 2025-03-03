c773769c678cb27347da7ea5e5bdbfdba8116c35
LIannhic <154102382+LIannhic@users.noreply.github.com>
2025-03-03 15:45:43 +0100

    last commit

--- main.js
+++ main.js
@@ -25,11 +25,38 @@ console.log(
   })
 );
 
+// Définition d'une fonction afficheAddresse modifiée 
+function afficheAddresseMod(address){
+  // Retourner une phrase formatée en utilisant la synthaxe d'interpolation les propriétés du paramètre 'address'
+  return `Votre adresse est : ${address.street}, ${address.city}, ${address.country}.`;
+}
+
+console.log("Solution 1.1"); // pour la lisibilité 
+console.log(
+  afficheAddresseMod({
+    street: "10 rue de la republique",
+    city: "Paris",
+    country: "France",
+  })
+);
+console.log(""); // pour la lisibilité
+
+
 // Exercice 1.2 : Utilise les chaînes multilignes avec les backticks ` pour afficher plus simplement le menu
 console.log("Exercice 1.2");
 console.log(
   "Menu de la semaine :\n    - Pâte à tartine\n    - Hoagie\n    - Salade de pommes"
 );
+
+// Chaîne de caractère transformé
+console.log("Solution 1.2");
+console.log(
+  `Menu de la semaine :
+    - Pâte à tartine
+    - Hoagie
+    - Salade de pommes`
+);
+console.log("");
 
 /**
  * Exercice 2
@@ -47,12 +74,25 @@ const yellow = colors[3];
 console.log("Exercice 2.1");
 console.log(red, green, blue, yellow);
 
+const [redMod, greenMod, blueMod, yellowMod] = colors;
+
+console.log("Solution 2.1");
+console.log(redMod, greenMod, blueMod, yellowMod);
+console.log("");
+
 // Exercice 2.2 : La même chose, mais en ignorant ici "Red" et "Yellow"
 const green2 = colors[1];
 const blue2 = colors[2];
 
 console.log("Exercice 2.2");
 console.log(green2, blue2);
+
+// Déclaration et affectation par décomposition en ignorant certaines valeur
+const [, green2Mod, blue2Mod, ]= colors;
+
+console.log("Solution 2.2");
+console.log(green2Mod, blue2Mod);
+console.log("");
 
 // ------ Version objet
 
@@ -86,6 +126,13 @@ const tome = roman.tome;
 console.log("Exercice 2.3");
 console.log(title, editor, tome);
 
+// 
+const {title: titleMod, editor: editorMod, tome: tomeMod} = roman;
+
+console.log("Solution 2.3");
+console.log(titleMod, editorMod, tomeMod);
+console.log("");
+
 // Exercice 2.4 : Décompose l'objet pour récupérer le nom du personnage principal,
 // le nom du premier personnage secondaire
 // et la voix Japonaise dans l'anime du second personnage secondaire
@@ -99,6 +146,17 @@ console.log(
   secondCharacterName,
   thirdCharacterJapaneseVoice
 );
+
+
+const {mainCharacter: {name: mainCharacterNameMod}, characters: [{name: secondCharacterNameMod},{animeJapaneseVoice: thirdCharacterJapaneseVoiceMod}]} = roman;
+
+console.log("Solution 2.4");
+console.log(
+  mainCharacterNameMod,
+  secondCharacterNameMod,
+  thirdCharacterJapaneseVoiceMod
+);
+console.log("");
 
 // ------
 
@@ -114,6 +172,16 @@ lastname = temp;
 console.log("Exercice 2.5");
 console.log(firstname, lastname);
 
+// Réaffection les ancienne valeurs des variables firstname et lastname
+firstname = "John";
+lastname = "Doe";
+// affectation par décomposition
+[firstname, lastname] = [lastname, firstname] // Merci chatgpt
+// affichage de la réponse
+console.log("Solution 2.5");
+console.log(firstname, lastname);
+console.log("");
+
 // ------
 const person = {
   firstname,
@@ -125,10 +193,19 @@ const person = {
 // Exercice 2.6 : Fais une décomposition du paramètre pour extraire directement le prénom et le nom
 function hello(person) {
   console.log(`Bonjour ${person.firstname} ${person.lastname} !`);
-}
+};
 
 console.log("Exercice 2.6");
 hello(person);
+
+// mettre un commentaire ici
+function helloMod({firstname, lastname}) {
+  console.log(`Bonjour ${firstname} ${lastname} !`);
+};
+
+console.log("Solution 2.6");
+helloMod(person);
+console.log("");
 
 /**
  * Exercice 3
@@ -144,17 +221,30 @@ restOfColors.push(colors[3]);
 console.log("Exercice 3.1");
 console.log(restOfColors);
 
+// rappel des valeurs dans le tableau colors ["Red", "Green", "Blue", "Yellow"]
+const [red3Mod, green3Mod, ...restOfColorsMod] = colors;
+
+console.log("Solution 3.1");
+console.log(restOfColorsMod);
+console.log("");
+
 // Exercice 3.2 : Décompose l'objet, de telle sorte à ce que le reste contienne seulement les clés
-// titre, editeur, tome, titre, auteur
+// titre, editeur, tome, auteur
 
 const roman2 = {};
 roman2.title = roman.title;
 roman2.editor = roman.editor;
 roman2.tome = roman.tome;
-roman2.auteur = roman.auteur;
+roman2.author = roman.author;
 
 console.log("Exercice 3.2");
 console.log(roman2);
+
+// rappel de l'objet roman {title:"Solo Leveling",editor:"kbooks",tome:12,type:"fantasy",author:"Chugong",mainCharacter:{name:"Sung Jinwoo",animeJapaneseVoice:"Taito Ban",},characters:[{name:"Sung Jinah",animeJapaneseVoice: "Haruna Mikawa",},{name:"Go Gunhee",animeJapaneseVoice:"Banjō Ginga",},],};
+const {type: typeMod, mainCharacter: mainCharacterMod, characters: charactersMod, ...restOfRomanMod} = roman
+console.log("Solution 3.2");
+console.log(restOfRomanMod);
+console.log("");
 
 // ------
 const randomNumbers = [12, 13, 52, 68, 2, 23, 5];
@@ -164,6 +254,10 @@ const randomNumbers = [12, 13, 52, 68, 2, 23, 5];
 console.log("Exercice 3.3");
 console.log(Math.min(12, 13, 52, 68, 2, 23, 5));
 
+console.log("Solution 3.3");
+console.log(Math.min(...randomNumbers));
+console.log("");
+
 // Exercice 3.4 : Transforme cette fonction pour qu'elle puisse prendre
 // un nombre indéfini de paramètres car actuellement elle en prend que 3
 function min(a, b, c) {
@@ -172,6 +266,15 @@ function min(a, b, c) {
 
 console.log("Exercice 3.4");
 console.log(min(12, 13, 52, 68, 2, 23, 5)); // Le résultat doit être 2
+
+// Définir une fonction minMod
+function minMod(...args) {
+  return Math.min(...args);
+};
+
+console.log("Solution 3.4");
+console.log(minMod(12, 13, 52, 68, 2, 23, 5,));
+console.log("");
 
 /**
  * Exercice 4
@@ -184,6 +287,14 @@ const moreColors = colors.concat("Pink", "Magenta");
 console.log("Exercice 4.1");
 console.log(moreColors);
 
+// ok
+const colorsMod = ["Red", "Green", "Blue", "Yellow"];
+const moreColorsMod = [...colorsMod, "Pink", "Magenta"];
+
+console.log("Solution 4.1");
+console.log(moreColorsMod);
+console.log("");
+
 // Exercice 4.2 : Utilise l'opérateur de dispersion pour créer un nouveau tableau
 // où Red est ajouté au début, et "Violet" à la fin
 const partialRainbow = ["Orange", "Yellow", "Green", "Blue", "Indigo"];
@@ -191,6 +302,14 @@ const rainbow = ["Red"].concat(partialRainbow, ["Violet"]);
 
 console.log("Exercice 4.2");
 console.log(rainbow);
+
+// 
+const partialRainbowMod = ["Orange", "Yellow", "Green", "Blue", "Indigo"];
+const rainbowMod = ["Red",...partialRainbowMod, "Violet"];
+
+console.log("Solution 4.2");
+console.log(rainbowMod);
+console.log("");
 
 // Exercice 4.3 : Copier l'adresse en ajoutant une nouvelle clé pour le code postal
 const adress = {
@@ -204,11 +323,31 @@ const completeAdress = Object.assign({}, adress, { postalCode: 75001 });
 console.log("Exercice 4.3");
 console.log(completeAdress);
 
+// Ma réponse
+const adressMod = {
+  streetMod: "10 rue de la republique",
+  cityMod: "Paris",
+  countryMod: "France",
+};
+
+const completeAdressMod = {...adressMod,  postalCode: 75001 };
+
+console.log("Solution 4.3");
+console.log(completeAdressMod);
+console.log("");
+
 // Exercice 4.4 : Utilise l'opérateur de dispersion pour copier l'objet person et modifier la valeur de l'age à 20
 const person2 = Object.assign({}, person, { age: 20 });
 
 console.log("Exercice 4.4");
 console.log(person2);
+
+// L'objet person a une propriété age à la valeur 30
+const person2Mod = { ...person,  age: 20 };
+
+console.log("Solution 4.4");
+console.log(person2Mod);
+console.log("");
 
 /**
  * Exercice 5
@@ -223,6 +362,15 @@ const thatsAllFolks = function () {
 console.log("Exercice 5.1");
 thatsAllFolks();
 
+// Heu... j'applique le cours
+const thatsAllFolksMod = () => {
+  console.log("That's all folks!");
+};
+
+console.log("Solution 5.1");
+thatsAllFolksMod();
+console.log("");
+
 // Exercice 5.2 : Convertir toutes les fonctions en fonction fléchée
 // Et si c'est possible, faire un return implicite
 const makeDouble = function (numbers) {
@@ -235,6 +383,17 @@ const makeDouble = function (numbers) {
 console.log("Exercice 5.2");
 console.log(makeDouble([1, 2, 3]));
 
+// 
+const makeDoubleMod = (numbers) => numbers.map((number) => {
+  // Retour implicite impossible, il y a deux consignes ?!
+  const newNumber = number * 2;
+  return newNumber;
+});
+
+console.log("Solution 5.2");
+console.log(makeDoubleMod([1, 2, 3]));
+console.log("");
+
 // Exercice 5.3 : Convertir en fonction fléchée avec un return implicite
 function convertToPerson(firstnameLastnameTuple) {
   return {
@@ -245,6 +404,16 @@ function convertToPerson(firstnameLastnameTuple) {
 
 console.log("Exercice 5.3");
 console.log(convertToPerson(["John", "Doe"]));
+
+// 
+const convertToPersonMod = (firstnameLastnameTuple) => ({
+    firstname: firstnameLastnameTuple[0],
+    lastname: firstnameLastnameTuple[1],
+  });
+
+console.log("Solution 5.3");
+console.log(convertToPersonMod(["John", "Doe"]));
+console.log("");
 
 /**
  * Exercice 6
@@ -263,6 +432,14 @@ function convertToPerson2([firstname, lastname]) {
 console.log("Exercice 6");
 console.log(convertToPerson2(["John", "Doe"]));
 
+//
+const convertToPerson2Mod = ([firstname, lastname]) => ({firstname,lastname,})
+
+console.log("Solution 6");
+console.log(convertToPerson2Mod(["John", "Doe"]));
+console.log("");
+
+
 /**
  * Exercice 7
  * Modules import / export
@@ -275,64 +452,67 @@ console.log(convertToPerson2(["John", "Doe"]));
 //
 // Migre ce code dans un module, importe le nécéssaire pour l'utiliser
 
-const cart = [];
+// const cart = [];
 
-function addToCart(productInfo, quantity = 1) {
-  const cartLine = findItemInCart(productInfo.product);
+// function addToCart(productInfo, quantity = 1) {
+//   const cartLine = findItemInCart(productInfo.product);
 
-  if (cartLine) {
-    cartLine.quantity += quantity;
-    return;
-  }
+//   if (cartLine) {
+//     cartLine.quantity += quantity;
+//     return;
+//   }
 
-  cart.push({ ...productInfo, quantity });
-}
+//   cart.push({ ...productInfo, quantity });
+// }
 
-function findItemInCart(productName) {
-  return cart.find((cartLine) => cartLine.product === productName);
-}
+// function findItemInCart(productName) {
+//   return cart.find((cartLine) => cartLine.product === productName);
+// }
 
-function updateCartQuantity(productName, quantity) {
-  const cartLine = findItemInCart(productName);
+// function updateCartQuantity(productName, quantity) {
+//   const cartLine = findItemInCart(productName);
 
-  if (!cartLine) {
-    addToCart(productName, quantity);
-    return;
-  }
+//   if (!cartLine) {
+//     addToCart(productName, quantity);
+//     return;
+//   }
 
-  cartLine.quantity = quantity;
-}
+//   cartLine.quantity = quantity;
+// }
 
-function computeTotal() {
-  return cart.reduce(
-    (total, cartLine) => total + cartLine.quantity * cartLine.price,
-    0
-  );
-}
+// function computeTotal() {
+//   return cart.reduce(
+//     (total, cartLine) => total + cartLine.quantity * cartLine.price,
+//     0
+//   );
+// }
 
-function displayAmount(amount) {
-  return Intl.NumberFormat("fr-FR", {
-    style: "currency",
-    currency: "EUR",
-  }).format(amount);
-}
+// function displayAmount(amount) {
+//   return Intl.NumberFormat("fr-FR", {
+//     style: "currency",
+//     currency: "EUR",
+//   }).format(amount); 
+// }
+
+import exercice from "./exercice.js"
 
 console.log("Exercice 7");
 
 console.log("Ajout d'un Shampoing x2 à 10€");
-addToCart({ product: "Shampoing", price: 10 }, 2);
+exercice.addToCart({ product: "Shampoing", price: 10 }, 2);
 
 console.log("Mise à jour du panier : Shampoing x4");
-updateCartQuantity("Shampoing", 4);
+exercice.updateCartQuantity("Shampoing", 4);
 
 console.log("Ajout d'un Biscuit x10 à 3€");
-addToCart({ product: "Biscuit", price: 3 }, 10);
+exercice.addToCart({ product: "Biscuit", price: 3 }, 10);
 
 console.log("Ajout d'un Biscuit x2 à 3€");
-addToCart({ product: "Biscuit", price: 3 }, 2);
+exercice.addToCart({ product: "Biscuit", price: 3 }, 2);
 
 console.log("Affichage du total panier :");
-console.log(displayAmount(computeTotal()));
+console.log(exercice.displayAmount(exercice.computeTotal()));
+console.log("");
 
 /**
  * Exercice 8
@@ -342,7 +522,7 @@ console.log(displayAmount(computeTotal()));
 // Exercice 8 : Transforme les opérations asynchrone avec la syntaxe async/await
 
 function loadFruits() {
-  return fetch("http://127.0.0.1:5500/data/fruits.json").then((response) =>
+  return fetch("http://127.0.0.1:3000/data/fruits.json").then((response) =>
     response.json()
   );
 }
@@ -357,3 +537,40 @@ function program() {
 
 console.log("Exercice 8");
 program();
+
+// Définition d'une fonction asynchrone pour charger les fruits depuis un fichier JSON
+async function loadFruitsMod() {
+  try {
+    // Envoie une requête HTTP pour récupérer le fichier "fruits.json"
+    const response = await fetch("http://127.0.0.1:3000/data/fruits.json");
+
+    // Convertit la réponse en format JSON et la retourne
+    return await response.json();
+  } catch (error) {
+    // Si une erreur survient (ex : problème réseau, fichier introuvable...), on l'affiche
+    console.error("Erreur lors du chargement des fruits :", error);
+
+    // On relance l'erreur pour que la fonction appelante puisse aussi la gérer
+    throw error;
+  }
+}
+
+// Définition d'une fonction asynchrone pour exécuter le programme
+async function programMod() {
+  try {
+    // Appelle la fonction loadFruits() et attend que les données soient chargées
+    const fruits = await loadFruitsMod();
+
+    // Affiche la liste des fruits dans la console
+    console.log(fruits);
+  } catch (error) {
+    // Si une erreur survient, on affiche un message d'erreur
+    console.log("Problème lors du chargement des fruits", error);
+  }
+}
+
+// Affiche un message pour indiquer le début de l'exercice
+setTimeout(()=>{
+  console.log("Solution 8");
+  // Appelle la fonction principale du programme
+  programMod()}, 1000);
